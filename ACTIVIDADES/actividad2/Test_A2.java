e71ed3b84524c2a5cba51abc37dbfda982cb676c
Gaboootj10 <gabrielgotj@gmail.com>
2025-05-10 23:55:23 -0500

    actividades

---
+++ ACTIVIDADES/actividad2/Test_A2.java
@@ -0,0 +1,27 @@
+package Actividad2;
+
+import Actividad1.ExceptionIsEmpty;
+
+public class Test_A2 {
+    public static void main(String[] args) {
+        try {
+            Queue<Integer> cola = new QueueLink<>();
+
+            System.out.println("¿Está vacía?: " + cola.isEmpty());
+
+            cola.enqueue(10);
+            cola.enqueue(20);
+            cola.enqueue(30);
+
+            System.out.println("Cola actual: " + cola);
+            System.out.println("Frente: " + cola.front());
+            System.out.println("Final: " + cola.back());
+
+            System.out.println("Eliminando: " + cola.dequeue());
+            System.out.println("Cola después de eliminar: " + cola);
+
+        } catch (ExceptionIsEmpty e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
